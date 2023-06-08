29892a015703226081ace64c99f4cfed905a6a8f
mirza-emad-baig <emad.baig.mirza@gmail.com>
2023-06-08 23:44:39 +0400

    disable dark mode

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -8,14 +8,14 @@ export const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
         <header>
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-5 dark:bg-gray-800">
+            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-5 -dark:bg-gray-800">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link href="/" className="flex items-center">
                         <Logo />
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link href="/demo"
-                           className="hidden sm:block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get
+                           className="hidden sm:block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 -dark:bg-blue-600 -dark:hover:bg-blue-700 focus:outline-none -dark:focus:ring-blue-800">Get
                             started</Link>
                         <button className="block lg:hidden group space-y-1 hover:cursor-pointer"
                                 aria-controls="mobile-menu-2" aria-expanded="false"
@@ -31,19 +31,19 @@ export const Header = () => {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <Link href="/"
-                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Home</Link>
+                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 -dark:text-gray-400 lg:dark:hover:text-white -dark:hover:bg-gray-700 -dark:hover:text-white lg:dark:hover:bg-transparent -dark:border-gray-700">Home</Link>
                             </li>
                             <li>
                                 <Link href="/marketplace"
-                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Marketplace</Link>
+                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 -dark:text-gray-400 lg:dark:hover:text-white -dark:hover:bg-gray-700 -dark:hover:text-white lg:dark:hover:bg-transparent -dark:border-gray-700">Marketplace</Link>
                             </li>
                             <li>
                                 <a href="#"
-                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Search</a>
+                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 -dark:text-gray-400 lg:dark:hover:text-white -dark:hover:bg-gray-700 -dark:hover:text-white lg:dark:hover:bg-transparent -dark:border-gray-700">Search</a>
                             </li>
                             <li>
                                 <a href="#"
-                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 -dark:text-gray-400 lg:dark:hover:text-white -dark:hover:bg-gray-700 -dark:hover:text-white lg:dark:hover:bg-transparent -dark:border-gray-700">About</a>
                             </li>
                         </ul>
                     </div>
--- app/components/toaster.tsx
+++ app/components/toaster.tsx
@@ -9,10 +9,10 @@ export default function Toaster({type = 'error', message, onClose}: any) {
 
     return (
         <div
-            className="fixed right-0 left-0 mx-auto z-10 mt-8 flex items-center w-full max-w-xs p-4 mb-4 border text-gray-500 bg-white rounded-lg shadow-lg dark:text-gray-400 dark:bg-gray-800">
+            className="fixed right-0 left-0 mx-auto z-10 mt-8 flex items-center w-full max-w-xs p-4 mb-4 border text-gray-500 bg-white rounded-lg shadow-lg -dark:text-gray-400 -dark:bg-gray-800">
             {TOASTER_TYPES.ERROR === type && (
                 <div
-                    className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+                    className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg -dark:bg-red-800 -dark:text-red-200">
                     <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                          xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd"
@@ -25,7 +25,7 @@ export default function Toaster({type = 'error', message, onClose}: any) {
             <div className="ml-3 text-sm font-normal">{message}</div>
             <button type="button"
                     onClick={onClose}
-                    className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                    className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 -dark:text-gray-500 -dark:hover:text-white -dark:bg-gray-800 -dark:hover:bg-gray-700"
                     data-dismiss-target="#toast-danger" aria-label="Close">
                 <span className="sr-only">Close</span>
                 <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
