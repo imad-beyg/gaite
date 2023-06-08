41f37e8359f82b181e782785ea23a6551076d643
mirza-emad-baig <emad.baig.mirza@gmail.com>
2023-06-08 23:47:16 +0400

    disable dark mode

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
         <header>
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-5 dark:bg-gray-800">
+            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-5 -dark:bg-gray-800">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link href="/" className="flex items-center">
                         <Logo />
